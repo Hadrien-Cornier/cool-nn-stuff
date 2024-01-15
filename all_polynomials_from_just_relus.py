cd072b5407ad97e63931cba63f7255b66ef747bd
Hadrien Cornier <hcornier@talroo.com>
2024-01-15 12:25:40 -0600

    make the repeated waves happen

--- all_polynomials_from_just_relus.py
+++ all_polynomials_from_just_relus.py
@@ -26,24 +26,37 @@ def triangle_from_3_relus(x: np.ndarray) -> np.ndarray:
 
 def triangle_from_2_relus(x: np.ndarray) -> np.ndarray:
     # optimal in terms of number of neurons necessary
-    return relu(x) - 2 * relu(x - 0.5)
+    return 0.5 * (relu(x) - 2 * relu(x - 0.5))
 
 
-def plot(x: np.ndarray, y: np.ndarray, title: str = ""):
-    plt.plot(x, y)
-    plt.title(title)
-    plt.show()
+def plot(x: np.ndarray, y: np.ndarray, title: str = "", color: str = "blue"):
+    plt.plot(x, y, label=title, color=color)
 
 
-def apply_n(fn: Callable[[np.ndarray], np.ndarray], x: np.ndarray, n: int) -> np.ndarray:
-    y = x
-    for i in range(n):
-        y = fn(y)
-    return y
+def apply_n(fn: Callable[[np.ndarray], np.ndarray], n: int) -> Callable[[np.ndarray], np.ndarray]:
+    return lambda x: apply_n(fn, n - 1)(fn(x)) if n > 0 else x
+
+
+def double_triangle(x: np.ndarray) -> np.ndarray:
+    return triangle_from_2_relus(2 * triangle_from_2_relus(2 * x)) + triangle_from_2_relus(
+        2 * triangle_from_2_relus(2 * (x - 0.5)))
+
+
+def square_fractal(fn: Callable[[np.ndarray], np.ndarray]) -> Callable[[np.ndarray], np.ndarray]:
+    # obtained through trial and error
+    # creates repeated triangles
+    return lambda x: fn(2 * fn(2 * x)) + fn(2 * fn(2 * (x - 0.5)))
 
 
 if __name__ == "__main__":
-    X = np.linspace(0.0, 1.0, 101)
-    N = 4
-    Y = apply_n(triangle_from_2_relus, X, N)
-    plot(X, Y, f"triangle^{N}")
+    X = np.linspace(0.0, 1.0, 202)
+    N = 2
+    plot(X, triangle_from_2_relus(X), f"simple_triangle", color="red")
+    Y = square_fractal(square_fractal(triangle_from_2_relus))(X)  # apply_n(triangle_from_2_relus, X, N)
+    Y = apply_n(square_fractal,N)(triangle_from_2_relus)(X)
+    plot(X, Y, f"double_triangle", color="blue")
+    plt.legend(loc='best')
+    plt.title("repeated triangle_from_2_relus")
+    plt.show()
+
+# plot both of those on the same graph with different colors
