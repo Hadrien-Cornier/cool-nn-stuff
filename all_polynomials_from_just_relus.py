3e5aef4583acc2042ea303ac2ab98c7d74237f5f
Hadrien Cornier <hcornier@talroo.com>
2024-01-16 01:47:31 -0600

    make the problem more simple

--- all_polynomials_from_just_relus.py
+++ all_polynomials_from_just_relus.py
@@ -8,67 +8,80 @@ matplotlib.use('MacOSX')  # Replace 'TkAgg' with your preferred backend
 
 # The goal is to express all polynomials with a neural network that only uses relu activation functions
 # many thanks to Nikolaj-K @ https://www.youtube.com/watch?v=PApGm1TKFHQ for the topic
+# rewrote it in a different way
 
-def relu(x: np.ndarray) -> np.ndarray:
+def R(x: np.ndarray) -> np.ndarray:
+    """
+    Relu function
+    :param x: 1D array in R^n
+    :return: Relu(x) 1D array in R^n
+    """
     return np.maximum(x, 0)
 
 
-def triangle(x: np.ndarray) -> np.ndarray:
-    return x - 2 * relu(x - 0.5)
+def T(x: np.ndarray) -> np.ndarray:
+    """
+    Triangle function
+    """
+    return -R(x) - R(-x) + 1. / 2
 
 
-def identity_from_relus(x: np.ndarray) -> np.ndarray:
-    return relu(x) - relu(x - 1)
+def M(t: Callable[[np.ndarray], np.ndarray]) -> Callable[[np.ndarray], np.ndarray]:
+    """
+    Minification function (takes a triangle T and turns it into a double triangle M(T))
+    :param t: the triangle function
+    :return:
+    """
+    return lambda x: t(2 * x + 1) + t(2 * x - 1)
 
 
-def triangle_from_3_relus(x: np.ndarray) -> np.ndarray:
-    # used 3 relus => not optimal because it can be done in two relus
-    return identity_from_relus(x) - 2 * relu(x - 0.5)
+def plot(X: np.ndarray, Y: np.ndarray, label: str, color: str) -> None:
+    """
+    Plot a function
+    :param X: x values
+    :param Y: y values
+    :param label: label for the plot
+    :param color: color for the plot
+    :return: None
+    """
+    plt.plot(X, Y, label=label, color=color)
 
 
-def triangle_from_2_relus(x: np.ndarray) -> np.ndarray:
-    # optimal in terms of number of neurons necessary
-    return 0.5 * (relu(x) - 2 * relu(x - 0.5))
+def set_grid() -> None:
+    """
+    Set the grid for the plot
+    :return: None
+    """
+    plt.grid(True)
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+    plt.xlim(-0.5, 0.5)
+    plt.ylim(-0.5, 0.5)
 
-
-def plot(x: np.ndarray, y: np.ndarray, title: str = "", color: str = "blue"):
-    plt.plot(x, y, label=title, color=color)
-
-
-def apply_n(fn: Callable[[np.ndarray], np.ndarray], n: int) -> Callable[[np.ndarray], np.ndarray]:
-    return lambda x: apply_n(fn, n - 1)(fn(x)) if n > 0 else x
-
-
-def double_triangle(x: np.ndarray) -> np.ndarray:
-    # obtained through trial and error
-    # creates two peaks
-    return triangle_from_2_relus(2 * triangle_from_2_relus(2 * x)) + triangle_from_2_relus(
-        2 * triangle_from_2_relus(2 * (x - 0.5)))
-
-
-def square_fractal(fn: Callable[[np.ndarray], np.ndarray]) -> Callable[[np.ndarray], np.ndarray]:
-    # obtained through double_triangle
-    return lambda x: fn(2*fn(x))
-
-
-def express_square_fractal_in_terms_of_fnx(fn: Callable[[np.ndarray], np.ndarray]) -> Callable[[np.ndarray], np.ndarray]:
-    # obtained through double_triangle
-    return lambda x: fn(fn(x)) + fn(fn(x - 0.5))
-
-
-def real_square(fn: Callable[[np.ndarray], np.ndarray]) -> Callable[[np.ndarray], np.ndarray]:
-    return lambda x: fn(fn(x)) + fn(fn(x - 0.5))
 
 if __name__ == "__main__":
-    color_cycle = plt.cm.get_cmap('tab10')
+    # Our map is the square of all x,y s.t. x in [-0.5,0.5] and y in [0.0,0.5]
+    # This is shifted from Nikolaj-K's video where he uses [0,1]x[0,1]
+    # imo this makes it easier to understand
 
-    for N in range(1, 4):
-        X = np.linspace(0.0, 1.0, 10000)
-        Y = apply_n(square_fractal, N)(triangle_from_2_relus)(X)
-        plot(X, Y, f"order {N}", color=color_cycle(N))
+    # We want to express the square function in terms of relus and linear combinations
+    # essentiually we want to see -x**2+0.5
+    color_cycle = matplotlib.colormaps["viridis"]
+    X = np.linspace(-0.5, 0.5, 10000)
+    Y = T(X)
+    assert Y.max() <= 0.5 and Y.min() >= -0.5
+    plot(X, T(X), "$T_1$", color=color_cycle(0.0))
+    plot(X, T(2 * X + 1), "$T(2 * X + 1)$", color=color_cycle(0.25))
+    plot(X, T(2 * X - 1), "$T(2 * X - 1)$", color=color_cycle(0.5))
+    plot(X, -T(2 * X - 1), "$T(2 * X - 1)$", color=color_cycle(0.75))
+    plot(X, -T(-2 * X - 1), "$T(2 * X - 1)$", color=color_cycle(1.0))
+    plot(X, -2*X**2 + 0.5, "$-2*X^2+1/2$", color=color_cycle(0.9))
 
+    # print()
+    # plot(X, M(T)(X), "$T_2$", color=color_cycle(0.5))
+    set_grid()
     plt.legend(loc='best')
-    plt.title("repeated relus")
+    plt.title("getting $-2*X^2+1/2$ from Relus is the goal")
     plt.show()
 
 # plot both of those on the same graph with different colors
